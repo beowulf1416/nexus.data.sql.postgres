f793ce02db6ca6691404279968110d2b16b724f6
ferd <beowulf1416@gmail.com>
2025-11-02 12:28:05 +0800

    modify init for commons

--- sql/postgres/schemas/core/common/init.sql
+++ sql/postgres/schemas/core/common/init.sql
@@ -13,3 +13,30 @@ insert into dimensions(id, name) values
 (3, 'volume'),
 (4, 'weight')
 ;
+
+
+
+
+-- https://www.postgresql.org/docs/12/app-psql.html
+-- countries
+\copy countries (official_name_en,iso_3166_1_alpha_2,iso_3166_1_alpha_3,iso_3166_1_numeric,iso_4217_currency_alphabetic_code,iso_4217_currency_country_name,iso_4217_currency_minor_unit,iso_4217_currency_name,iso_4217_currency_numeric_code) from '/docker-entrypoint-initdb.d/csv/countries.csv' with delimiter ',' csv header quote '"'
+
+
+
+-- currencies
+-- temporarily create sequence to add records to currencies table
+create sequence if not exists seq_currency as int
+;
+
+alter table if exists common.currencies
+alter column id set default nextval('seq_currency')
+;
+
+\copy currencies (currency, unit_text, symbol) from '/docker-entrypoint-initdb.d/csv/currencies.csv' with delimiter ',' csv header quote '"'; 
+
+alter table if exists currencies
+alter column id drop default
+;
+
+drop sequence seq_currency
+;
