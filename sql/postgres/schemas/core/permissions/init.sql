3ef1a56938eb42440e38c0d23218525f4138cc70
ferd <beowulf1416@gmail.com>
2025-12-11 20:34:22 +0800

    add permissions. modify loading of permissions.

--- sql/postgres/schemas/core/permissions/init.sql
+++ sql/postgres/schemas/core/permissions/init.sql
@@ -3,4 +3,21 @@ set schema 'permissions';
 
 -- https://www.postgresql.org/docs/12/app-psql.html
 -- permissions
-\copy permissions (id,name,description) from '/docker-entrypoint-initdb.d/csv/permissions.csv' with delimiter ',' csv header quote '"'
+
+-- temporarily create sequence to add records to permissions table
+create sequence if not exists seq_permissions as int
+;
+
+alter table if exists permissions
+alter column id set default nextval('seq_permissions')
+;
+
+
+\copy permissions (name,description) from '/docker-entrypoint-initdb.d/csv/permissions.csv' with delimiter ',' csv header quote '"'
+
+alter table if exists permissions
+alter column id drop default
+;
+
+drop sequence seq_permissions
+;
