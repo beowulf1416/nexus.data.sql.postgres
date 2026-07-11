7088ab900efb866bb072e1715e49eb3063e20e75
ferd tomale <beowulf1416@gmail.com>
2026-07-11 12:03:55 +0800

    add method to fetch accounts filtered

--- sql/postgres/schemas/optional/acctg/procs.sql
+++ sql/postgres/schemas/optional/acctg/procs.sql
@@ -4,6 +4,8 @@ set schema 'acctg';
 \ir procs/account_types/account_types_fetch.sql
 \ir procs/account_categories/account_categories_fetch.sql
 \ir procs/accounts/accounts_fetch_all.sql
+\ir procs/accounts/accounts_fetch.sql
+
 \ir procs/accounts/account_save.sql
 
 \ir procs/invoice_types/invoice_types_fetch.sql
---
+++ sql/postgres/schemas/optional/acctg/procs/accounts/accounts_fetch.sql
@@ -0,0 +1,34 @@
+create or replace function accounts_fetch(
+    p_tenant_id uuid,
+    p_filter text default '%'
+)
+returns table(
+    account_id accounts.account_id%type,
+    active accounts.active%type,
+    account_type_id accounts.account_type_id%type,
+    account_category_id accounts.account_category_id%type,
+    name accounts.name%type,
+    code accounts.code%type,
+    description accounts.description%type
+)
+language plpgsql
+as $$
+begin
+    return query
+    select
+        a.account_id,
+        a.active,
+        a.account_type_id,
+        a.account_category_id,
+        a.name,
+        a.code,
+        a.description
+    from acctg.accounts a
+    where
+        a.tenant_id = p_tenant_id
+        and a.name ilike p_filter
+    ;
+end;
+$$;
+
+comment on function accounts_fetch is 'Fetches accounts based on tenant and filter criteria.';
