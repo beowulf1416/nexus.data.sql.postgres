c7c89ac5ceb3a6f1a68b99ad57210a8bb281d279
ferd tomale <beowulf1416@gmail.com>
2026-07-13 22:19:56 +0800

    create skeleton accounts for new tenants

--- sql/postgres/schemas/core/tenants/procs/tenants/tenant_save.sql
+++ sql/postgres/schemas/core/tenants/procs/tenants/tenant_save.sql
@@ -5,6 +5,12 @@ create or replace procedure tenant_save(
 )
 language plpgsql
 as $$
+declare
+    acctg_asset_id acctg.accounts.account_id%type;
+    acctg_liability_id acctg.accounts.account_id%type;
+    acctg_equity_id acctg.accounts.account_id%type;
+    acctg_expense_id acctg.accounts.account_id%type;
+    acctg_income_id acctg.accounts.account_id%type;
 begin
     insert into tenants.tenants (
         tenant_id,
@@ -21,7 +27,13 @@ begin
         description = p_description
     ;
 
-    -- insert this tenant's root account
+    -- insert this tenant skeleton accounting accounts
+    acctg_asset_id := public.gen_random_uuid();
+    acctg_liability_id := public.gen_random_uuid();
+    acctg_equity_id := public.gen_random_uuid();
+    acctg_expense_id := public.gen_random_uuid();
+    acctg_income_id := public.gen_random_uuid();
+
     insert into acctg.accounts (
         account_id,
         tenant_id,
@@ -30,14 +42,27 @@ begin
         name,
         description,
         code
-    ) values (
+    )
+    select
+        a.account_id,
         p_tenant_id,
-        p_tenant_id,
-        0,
-        0,
-        concat('root-', p_name),
-        concat('root-', p_name),
-        '0'
+        a.account_type_id,
+        a.account_category_id,
+        a.name,
+        a.description,
+        ''
+    from unnest(array[
+        (acctg_asset_id, 1::smallint, 1::smallint, 'Asset'::text, 'Asset'::text),
+        (acctg_liability_id, 2::smallint, 1::smallint, 'Liability'::text, 'Liability'::text),
+        (acctg_equity_id, 3::smallint, 1::smallint, 'Equity'::text, 'Equity'::text),
+        (acctg_expense_id, 4::smallint, 1::smallint, 'Expense'::text, 'Expense'::text),
+        (acctg_income_id, 5::smallint, 1::smallint, 'Income'::text, 'Income'::text)
+    ]) as a (
+        account_id uuid,
+        account_type_id smallint,
+        account_category_id smallint,
+        name varchar(100),
+        description varchar(300)
     )
     on conflict (account_id) do nothing
     ;
