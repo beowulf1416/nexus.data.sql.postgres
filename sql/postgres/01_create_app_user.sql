104bcf906f6d7fed1c69233267362bdbb7a3230d
ferd tomale <beowulf1416@gmail.com>
2026-07-21 12:37:00 +0800

    grant additional permissions

--- sql/postgres/01_create_app_user.sql
+++ sql/postgres/01_create_app_user.sql
@@ -1,17 +1,37 @@
 create user app_user with password 'replace_me';
 
 
+grant usage on schema common to app_user;
 grant select on all tables in schema common to app_user;
+
+grant usage on schema permissions to app_user;
 grant select on all tables in schema permissions to app_user;
 
+grant usage on schema auth to app_user;
 grant select, insert, update on all tables in schema auth to app_user;
+
 -- grant select, insert, update on all tables in schema documents to app_user;
+
+grant usage on schema files to app_user;
 grant select, insert, update on all tables in schema files to app_user;
+
+grant usage on schema organizations to app_user;
 grant select, insert, update on all tables in schema organizations to app_user;
+
+grant usage on schema tenants to app_user;
 grant select, insert, update on all tables in schema tenants to app_user;
+
+grant usage on schema users to app_user;
 grant select, insert, update on all tables in schema users to app_user;
+
+grant usage on schema acctg to app_user;
 grant select, insert, update on all tables in schema acctg to app_user;
+
+grant usage on schema crm to app_user;
 grant select, insert, update on all tables in schema crm to app_user;
+
+grant usage on schema user_registration to app_user;
 grant select, insert, update on all tables in schema user_registration to app_user;
+
 
 revoke insert, update on auth.auth_types from app_user;
